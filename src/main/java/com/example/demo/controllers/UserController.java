c6196fe098904a548d07e8fe68b12747f60a060f
UrosTim <urostimapps@gmail.com>
2024-07-10 11:21:43 +0200

    UserRepository, User entity, UserMapper and UserPageModel

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,26 +1,38 @@
 package com.example.demo.controllers;
 
+import com.example.demo.mappers.UserMapper;
 import com.example.demo.models.UserModel;
+import com.example.demo.models.UserPageModel;
+import com.example.demo.repositories.IUserRepository;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RequestMapping("user")
 @RestController
+@RequiredArgsConstructor
+@CrossOrigin("*")
 public class UserController {
+
+    private final IUserRepository userRepository;
 
     @GetMapping("get-first-name")
     public String getFirstName() {
         return "John";
     }
 
-    @GetMapping("get-first-name-list")
-    public List<String> getFirstNameList() {
-        return List.of("John", "Jane");
+    @GetMapping("get-user-list")
+    public List<UserModel> getUserList() {
+        return UserMapper.toModelList(userRepository.findAll());
+    }
+    @GetMapping("get-user-page-list")
+    public UserPageModel getUserPageList(Integer pageNumber, Integer pageSize) {
+        return UserMapper.toModelPagedList(userRepository.findAll(PageRequest.of(pageNumber, pageSize)));
     }
 
     @PostMapping("create-user")
@@ -33,6 +45,9 @@ public class UserController {
 //            return ResponseEntity.badRequest().body(result);
             return new ResponseEntity<>("Registration failed.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
+        var entity = UserMapper.toEntity(userModel);
+        userRepository.save(entity);
 
         return new ResponseEntity<UserModel>(userModel, HttpStatus.CREATED);
     }
